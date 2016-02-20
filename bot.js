c6b0965d5a1b5ef85741151c8922be16d45a8cd8
tdurham721 <tyler.durham721@gmail.com>
2016-02-20 01:58:51 -0500

    Added functions for rage meter. Need to update with instructions to push statistics to the server. Also need to research the fastest way to check if a string matches a string in an array in javascript since hashmaps do not exist.

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 var settings =
         {
-            channels : ["removed"],
+            channels : ["#thabeast721"],
             server : "irc.twitch.tv",
             port: 6667,
             secure: false,
-            nick : "remove",
-            password : "removed"
+            nick : "Super_Happy_Bot",
+            password : "oauth:slbgkt15b6ryl7eh0k6h3ctxcz2gkq"
         };
 
 var options = 
@@ -13,10 +13,31 @@ var options =
         host: "warp.world"
     };
 
-
-
 var irc = require("irc");
 var http = require("http");
+
+var botCommands = [/!add ([a-z0-9]{4}-){3}[a-z0-9]{4}/i,
+                   /!close/i,
+                   /!completed/i,
+                   /!current/i,
+                   /!list/i,
+                   /!next/i,
+                   /!open/i,
+                   /!queue/i,
+                   /!remove ([a-z0-9]{4}-){3}[a-z0-9]{4}/i,
+                   /!skip/i,
+                   /!stats/i,
+                   /!submit( ([a-z0-9]{4}-){3}[a-z0-9]{4})?/i,
+                   /!initiaterage "[a-z0-9]+" "[a-z0-9]+"/i,
+                   /!stoprage/i];
+
+function UserRage(username, answer)
+    {
+        this.username = name;
+        this.answer = answer;
+    }
+
+var listOfUserRage = [];
 
 var bot = new irc.Client(settings.server, settings.nick,
     {
@@ -25,7 +46,6 @@ var bot = new irc.Client(settings.server, settings.nick,
         password: settings.password,
         username: settings.nick
     });
-
 
 bot.addListener("join", function(channel, who)
     {
@@ -44,6 +64,7 @@ bot.addListener("message", remove);
 bot.addListener("message", skip);
 bot.addListener("message", stats);
 bot.addListener("message", submit);
+bot.addListener("message", trackTF);
 
 function makeHTTPCall()
 {
@@ -56,6 +77,58 @@ function makeHTTPCall()
         } );
     } );
 };
+
+function intitiateRage(from, to, message)
+{
+    var RE = /!initiaterage "[a-z0-9]+" "[a-z0-9]+"/i;
+    if (message.match(RE) != null)
+    {
+        listOfUserRage = [];
+        var messageCopy = message;
+        var firstWord = messageCopy.substring(15, message.indexOf("\""));
+        messageCopy = messageCopy.substring(messageCopy.indexOf("\"") + 3);
+        var secondWord = messageCopy.substring(0, messageCopy.indexOf("\""));
+        var postToServer = setInterval(trackRage(firstWord, secondWord, listOfUserRage, 5000));
+    }
+    if (message.match(/!stoprage/i) != null)
+    {
+        clearInterval(postToServer);
+    }
+}
+
+function trackRage(firstWord, secondWord, listOfUserRage)
+{
+    bot.addListener("message", function(from, to, message)
+    {
+        var answer = 0;
+        var alreadyEntered = false;
+        if (message === firstWord)
+        {
+            answer = 1;
+        }
+        else if (message === secondWord)
+        {
+            answer = 2;
+        }
+        if (answer != 0)
+        {
+            let iterable = listOfUserRage;
+            for (let entry of iterable)
+            {
+                if (entry.username === from && entry.answer === answer)
+                {
+                    alreadyEntered = true;
+                    break;    
+                }
+            }
+            if (!alreadyEntered)
+            {
+                listOfUserRage.push(new UserRage(from, answer));
+            }
+        }
+    } );
+    //post info to server here. need to figure out how to do this
+}
 
 function add(from, to, message)
 {
@@ -96,6 +169,7 @@ function current(from, to, message)
     var RE = /!current/i;
     if (message.match(RE) != null)
     {
+        console.log(from);
         var streamerName = to.substring(1);
         options.path = "/bot/current?streamer=" + streamerName + "&submitter=" + from;
         makeHTTPCall();
