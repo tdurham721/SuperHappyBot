426a47b54ef2e4b0fb24ef994cfae5b213c61213
tdurham721 <tyler.durham721@gmail.com>
2016-02-20 20:48:13 -0500

    Removed duplicate code for the queue functions. Now only contains one listener for the bot, and the bot determines which command to execute based on an array of commands. Functionality for the rage function does not yet work. The code needs fixed first.

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var settings =
             port: 6667,
             secure: false,
             nick : "Super_Happy_Bot",
-            password : "oauth:slbgkt15b6ryl7eh0k6h3ctxcz2gkq"
+            password : "oauth:r4tbb19ogfip1r8qrkq227nsg1cxfa"
         };
 
 var options = 
@@ -16,20 +16,22 @@ var options =
 var irc = require("irc");
 var http = require("http");
 
-var botCommands = [/!add ([a-z0-9]{4}-){3}[a-z0-9]{4}/i,
-                   /!close/i,
-                   /!completed/i,
-                   /!current/i,
-                   /!list/i,
-                   /!next/i,
-                   /!open/i,
-                   /!queue/i,
-                   /!remove ([a-z0-9]{4}-){3}[a-z0-9]{4}/i,
-                   /!skip/i,
-                   /!stats/i,
-                   /!submit( ([a-z0-9]{4}-){3}[a-z0-9]{4})?/i,
-                   /!initiaterage "[a-z0-9]+" "[a-z0-9]+"/i,
-                   /!stoprage/i];
+var botCommands =
+[
+    { pattern: /!add ([a-z0-9]{4}-){3}[a-z0-9]{4}/i, command: add },
+    { pattern: /!close/i, command: close },
+    { pattern: /!completed/i, command: completed },
+    { pattern: /!current/i, command: current },
+    { pattern: /!list/i, command: list },
+    { pattern: /!next/i, command: next },
+    { pattern: /!open/i, command: open },
+    { pattern: /!queue/i, command: queue },
+    { pattern: /!remove ([a-z0-9]{4}-){3}[a-z0-9]{4}/i, command: remove },
+    { pattern: /!skip/i, command: skip },
+    { pattern: /!stats/i, command: stats },
+    { pattern: /!submit( ([a-z0-9]{4}-){3}[a-z0-9]{4})?/i, command: submit },
+    { pattern: /!initiaterage "[a-z0-9]+" "[a-z0-9]+"/i, command: initiateRage },
+];
 
 function UserRage(username, answer)
     {
@@ -52,19 +54,18 @@ bot.addListener("join", function(channel, who)
         console.log("joined " + channel);
     });
 
-bot.addListener("message", add);
-bot.addListener("message", close);
-bot.addListener("message", completed);
-bot.addListener("message", current);
-bot.addListener("message", list);
-bot.addListener("message", next);
-bot.addListener("message", open);
-bot.addListener("message", queue);
-bot.addListener("message", remove);
-bot.addListener("message", skip);
-bot.addListener("message", stats);
-bot.addListener("message", submit);
-bot.addListener("message", trackTF);
+bot.addListener("message", matchMessageToCommand);
+
+function matchMessageToCommand(from, to, message)
+{
+    for (var i = 0; i < botCommands.length; i++)
+    {
+        if (message.match(botCommands[i].pattern) != null)
+        {
+            botCommands[i].command(from, to, message);
+        }
+    }
+}
 
 function makeHTTPCall()
 {
@@ -78,18 +79,14 @@ function makeHTTPCall()
     } );
 };
 
-function intitiateRage(from, to, message)
+function initiateRage(from, to, message)
 {
-    var RE = /!initiaterage "[a-z0-9]+" "[a-z0-9]+"/i;
-    if (message.match(RE) != null)
-    {
-        listOfUserRage = [];
-        var messageCopy = message;
-        var firstWord = messageCopy.substring(15, message.indexOf("\""));
-        messageCopy = messageCopy.substring(messageCopy.indexOf("\"") + 3);
-        var secondWord = messageCopy.substring(0, messageCopy.indexOf("\""));
-        var postToServer = setInterval(trackRage(firstWord, secondWord, listOfUserRage, 5000));
-    }
+    listOfUserRage = [];
+    var messageCopy = message;
+    var firstWord = messageCopy.substring(15, message.indexOf("\""));
+    messageCopy = messageCopy.substring(messageCopy.indexOf("\"") + 3);
+    var secondWord = messageCopy.substring(0, messageCopy.indexOf("\""));
+    var postToServer = setInterval(trackRage(firstWord, secondWord, listOfUserRage), 5000);
     if (message.match(/!stoprage/i) != null)
     {
         clearInterval(postToServer);
@@ -112,18 +109,11 @@ function trackRage(firstWord, secondWord, listOfUserRage)
         }
         if (answer != 0)
         {
-            let iterable = listOfUserRage;
-            for (let entry of iterable)
+            
+            currentUserRage = new UserRage(from, answer);
+            if (!listOfUserRage.contains(currentUserRage))
             {
-                if (entry.username === from && entry.answer === answer)
-                {
-                    alreadyEntered = true;
-                    break;    
-                }
-            }
-            if (!alreadyEntered)
-            {
-                listOfUserRage.push(new UserRage(from, answer));
+                listOfUserRage.push(currentUserRage);
             }
         }
     } );
@@ -132,135 +122,100 @@ function trackRage(firstWord, secondWord, listOfUserRage)
 
 function add(from, to, message)
 {
-    var RE = /!add ([a-z0-9]{4}-){3}[a-z0-9]{4}/i;
-    if (message.match(RE) != null)
-        {
-            var streamerName = to.substring(1);
-            var levelCode = message.substring(5);
-            options.path = "/bot/add?streamer=" + streamerName + "&key=bba3e1c6fbe8e202&submitter=" + from + "&levelcode=" + levelCode;
-            makeHTTPCall();
-        };
-};
+    var streamerName = to.substring(1);
+    var levelCode = message.substring(5);
+    options.path = "/bot/add?streamer=" + streamerName + "&key=bba3e1c6fbe8e202&submitter=" + from + "&levelcode=" + levelCode;
+    makeHTTPCall();
+}
 
 function close(from, to, message)
 {
-    var RE = /!close/i;
-    if (message.match(RE) != null)
-        {
-            var streamerName = to.substring(1);
-            options.path = "/bot/close?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
-            makeHTTPCall();
-        }
+    var streamerName = to.substring(1);
+    options.path = "/bot/close?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
+    makeHTTPCall();
 }
 
 function completed(from, to, message)
 {
-    var RE = /!completed/i;
-    if (message.match(RE) != null)
-    {
-        var streamerName = to.substring(1);
-        options.path = "/bot/complete?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
-        makeHTTPCall();
-    }
+    var streamerName = to.substring(1);
+    options.path = "/bot/complete?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
+    makeHTTPCall();
 }
 
 function current(from, to, message)
 {
-    var RE = /!current/i;
-    if (message.match(RE) != null)
-    {
-        console.log(from);
-        var streamerName = to.substring(1);
-        options.path = "/bot/current?streamer=" + streamerName + "&submitter=" + from;
-        makeHTTPCall();
-    }
+    console.log(from);
+    var streamerName = to.substring(1);
+    options.path = "/bot/current?streamer=" + streamerName + "&submitter=" + from;
+    makeHTTPCall();
 }
 
 function list(from, to, message)
 {
-    var RE = /!list/i;
-    if (message.match(RE) != null)
-    {
-        var streamerName = to.substring(1);
-        options.path = "/bot/list?streamer=" + streamerName + "&submitter=" + from;
-        makeHTTPCall();
-    }
+    var streamerName = to.substring(1);
+    options.path = "/bot/list?streamer=" + streamerName + "&submitter=" + from;
+    makeHTTPCall();
 }
 
 function next(from, to, message)
 {
-    var RE = /!next/i;
-    if (message.match(RE) != null)
-    {
-        var streamerName = to.substring(1);
-        options.path = "/bot/next?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
-        makeHTTPCall();
-    }
+    var streamerName = to.substring(1);
+    options.path = "/bot/next?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
+    makeHTTPCall();
 }
 
 function open(from, to, message)
 {
-    var RE = /!open/i;
-    if (message.match(RE) != null)
-    {
-        var streamerName = to.substring(1);
-        options.path = "/bot/open?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
-        makeHTTPCall();
-    }
+    var streamerName = to.substring(1);
+    options.path = "/bot/open?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
+    makeHTTPCall();
 }
 
 function queue(from, to, message)
 {
-    var RE = /!queue/i;
-    if (message.match(RE) != null)
-    {
-        var streamerName = to.substring(1);
-        options.path = "/bot/queue?submitter=" + from + "&streamer=" + streamerName;
-        makeHTTPCall();
-    }
+    var streamerName = to.substring(1);
+    options.path = "/bot/queue?submitter=" + from + "&streamer=" + streamerName;
+    makeHTTPCall();
 }
 
 function remove(from, to, message)
 {
-    var RE = /!remove ([a-z0-9]{4}-){3}[a-z0-9]{4}/i;
-    if (message.match(RE) != null)
-    {
         var streamerName = to.substring(1);
         var levelCode = message.substring(8);
         options.path = "/bot/remove?streamer=" + streamerName + "&submitter=" + from + "&levelcode=" + levelCode + "&key=bba3e1c6fbe8e202";
         makeHTTPCall();
-    }
 }
 
 function skip(from, to, message)
 {
-    var RE = /!skip/i;
-    if (message.match(RE) != null)
-    {
-        var streamerName = to.substring(1);
-        options.path = "/bot/skip?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
-        makeHTTPCall();
-    }
+    var streamerName = to.substring(1);
+    options.path = "/bot/skip?streamer=" + streamerName + "&key=bba3e1c6fbe8e202";
+    makeHTTPCall();
 }
 
 function stats(from, to, message)
 {
-    var RE = /!stats/i;
-    if (message.match(RE) != null)
-    {
-        var streamerName = to.substring(1);
-        options.path = "/bot/stats?streamer=" + streamerName;
-        makeHTTPCall();
-    }
+    var streamerName = to.substring(1);
+    options.path = "/bot/stats?streamer=" + streamerName;
+    makeHTTPCall();
 }
 
 function submit(from, to, message)
 {
-    var RE = /!submit( ([a-z0-9]{4}-){3}[a-z0-9]{4})?/i
-    if (message.match(RE) != null)
+    var streamerName = to.substring(1);
+    options.path = "/bot/submit?streamer=" + streamerName + "&submitter=" + from;
+    makeHTTPCall();
+}
+
+Array.prototype.contains = function(obj)
+{
+    var i = this.length;
+    while (i--)
     {
-        var streamerName = to.substring(1);
-        options.path = "/bot/submit?streamer=" + streamerName + "&submitter=" + from;
-        makeHTTPCall();
+        if (this[i] === obj)
+        {
+            return true;
+        }
     }
+    return false;
 }
